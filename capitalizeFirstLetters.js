61620a6ad427412d74545514d0f24f4149c94748
RomainMouly <romainmouly@gmail.com>
2020-10-23 09:04:45 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,15 @@
 
 const assert = require('assert');
 
+function capitalizeFirstLetters(input) {
+    return input.length > 0 ?
+    input.split(' ').map(element => element[0].toUpperCase() + element.slice(1)).join(' ')
+    : '';
+    
+
+  }
+
+  capitalizeFirstLetters('javaScript with react');
 
 
 // Check that capitalizeFirstLetters is a function
@@ -8,7 +17,7 @@ assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 // Check that capitalizeFirstLetters accepts one argument
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 // Check that capitalizeFirstletters transforms javaScript correctly
-assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
+assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
 //Check plusieurs mots dans une phrase
 assert.strictEqual(capitalizeFirstLetters('javaScript with react'), 'JavaScript With React');
 // Check that it works for a 1-character string
